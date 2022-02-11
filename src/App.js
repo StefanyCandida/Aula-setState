7235e2bce7b5fd93179ce1321883d4be7284e71d
StefanyCandida <stefany.candida@gmail.com>
2022-02-11 15:57:00 -0300

    Aula setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* if 
+import React, {Component} from 'react'
+
+class State extends Component{
+
+  state={
+    nome: "Dan",
+    idade: 25,
+    cor: "azul"
+  }
+
+  mensagem = () => {
+    if(this.state.cor === "azul"){
+      this.setState({cor:"amarelo"})
+    }else{
+      this.setState({cor:"azul"})
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Minha cor favorita é {this.state.cor}</h1>
+        <button onClick={this.mensagem}>Trocar</button>
+        <button onClick={this.voltar}>Voltar</button>
+      </div>
+    )
+ 
+  }
+  
+
+export default State
+*/
+
+ /* : ? 
+import React, {Component} from 'react'
+
+class State extends Component{
+
+  state = {
+    nome: "lina",
+   gosto:"tranças"
+  }
+
+  mensagem = () => {
+    this.setState({
+    nome: this.state.cor === "Prior" ? "lina" : this.state.nome === "lina" ? "babu" : "prior",
+    gosto: this.state.gosto === "jogar y jogar" ? "tranças" : this.state.gosto === "tranças" ?"cozinhar" : "jogar y jogar"
+  });
+  };
+
+  render(){
+    return(
+      <div>
+        <h1>Minha cor favorita é {this.state.cor}</h1>
+        <button onClick={this.mensagem}>Trocar</button>
+        <button onClick={this.voltar}>Voltar</button>
+      </div>
+    )
+ 
+  }
+  
+
+export default State
+
+*/
+
+
+/*   onClick
+import React, {Component} from 'react'
+
+class State extends Component{
+
+  state={
+    nome: "Dan",
+    idade: 25,
+    cor: "azul"
+  }
+
+  mensagem = () => {
+    this.setState({
+      cor: "amarelo"
+    })
+  }
+
+
+  voltar = () => {
+    this.setState({
+      cor: "azul"
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Minha cor favorita é {this.state.cor}</h1>
+        <button onClick={this.mensagem}>Trocar</button>
+        <button onClick={this.voltar}>Voltar</button>
+      </div>
+    )
+ 
+  }
 }
 
-export default App;
+export default State
+
+
+*/
+
+
+/*
+import React, {Component}  from 'react'
+
+class MudaFoto extends Component {
+
+state={
+  nome: "Astro", 
+   idade: 21,
+   corFav:"azul"
+    }
+
+
+mensagem = () => {
+  this.setState({
+    cor:"Amarelo"
+  })
+}
+
+  render(){
+    return(
+      <div>
+
+  <h1>Minha cor favoria é {this.state.corFav}</h1>
+  <button>Clica aqui</button>
+     </div>
+)
+}}
+export default MudaFoto
+*/
